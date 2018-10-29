7284cdad89ee947e31a5d2f9f68ce0f742a62056
Box <bradycfox@gmail.com>
2018-10-29 14:36:32 -0400

    added user models

---
+++ models/user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  fullname: String,
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+});
+
+
+
+// Add `createdAt` and `updatedAt` fields
+userSchema.set('timestamps', true);
+
+// Transform output during `res.json(data)`, `console.log(data)` etc.
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+    delete result.password;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
